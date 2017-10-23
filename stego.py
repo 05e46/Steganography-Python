8842b2c9ece1bae70893cf1f69d5452137bb5b49
05e46 <phillip.perez44@gmail.com>
2017-10-23 13:22:54 -0700

    python testing file added

--- stego.py
+++ stego.py
@@ -1,9 +1,14 @@
 from PIL import Image
-import binascii
-import optparse
+from PIL import ImageFont
 
 #image object to work with
 #image1 = Image.open('m3.jpg')
 #image1.show()
 #image1.save('m3.png')
 
+
+image = Image.open('m3.jpg')
+fontType = ImageFont.load('arial.pil')
+draw = ImageDraw.Draw(image)
+draw.text(xy==(50,50),text='dont be a little bitch',fill=(255,60,0),font=fontType)
+image.show()
---
+++ test.py
@@ -0,0 +1,11 @@
+import random
+import sys
+import os
+from PIL import Image
+
+print('hello world')
+
+name = 'Felipe Perez'
+
+img = Image.open('m3.jpg')
+img.show()
